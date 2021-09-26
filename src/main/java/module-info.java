cb859001cea49e7f2a23e2582658a8c7f8937642
kane16 <lukasz.guminski629@gmail.com>
2021-09-26 14:08:03 +0200

    Slightly corrected dependencies.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module pl.delukesoft.jplotlibusecase {
   requires javafx.fxml;
 
   requires org.controlsfx.controls;
+  requires jplotlib;
 
   opens pl.delukesoft.jplotlibusecase to javafx.fxml;
   exports pl.delukesoft.jplotlibusecase;
--- src/main/java/pl/delukesoft/jplotlibusecase/MainController.java
+++ src/main/java/pl/delukesoft/jplotlibusecase/MainController.java
@@ -1,6 +1,8 @@
 package pl.delukesoft.jplotlibusecase;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
@@ -9,8 +11,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import pl.delukesoft.jplotlib.model.enums.PlotType;
 
-public class MainController{
+
+public class MainController {
 
   @FXML
   private Label JPlotLibWelcomeLabel;
@@ -37,22 +41,27 @@ public class MainController{
   private GridPane pane;
 
   @FXML
-  public void initialize(){
-    plotTypeChoice.getItems().addAll("STANDARD", "AGGREGATION");
+  public void initialize() {
+    plotTypeChoice.getItems()
+        .addAll(
+            Arrays.stream(PlotType.values())
+                    .map(PlotType::toString)
+                    .collect(Collectors.toList())
+        );
     aggregationTypeChoice.getItems().addAll("AVG", "SUM", "MAX");
   }
 
-  public void uploadCsv(){
+  public void uploadCsv() {
     FileChooser chooser = new FileChooser();
     chooser.setTitle("Choose CSV file");
-    chooser.getExtensionFilters().add(new ExtensionFilter("Comma separated values","*.csv"));
+    chooser.getExtensionFilters().add(new ExtensionFilter("Comma separated values", "*.csv"));
     File file = chooser.showOpenDialog(pane.getScene().getWindow());
-    if(file != null){
+    if (file != null) {
       System.out.printf("Chosen file: %s", file.getAbsolutePath());
       System.out.println();
       plotTypeChoice.setDisable(false);
       plotTypeChoice.setValue("STANDARD");
-    }else {
+    } else {
       System.out.println("File has not been chosen");
     }
   }
